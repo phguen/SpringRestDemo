8d09ec8804eae83ba40fe0410875b7c167cef10c
Phil <Phil@PHIL-PC>
2018-09-12 19:37:54 +0200

    stage 1

---
+++ src/main/java/com/phil/SpringRestDemo/DatabaseLoader.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.phil.SpringRestDemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Phil
+ */
+@Component
+public class DatabaseLoader implements CommandLineRunner{
+    
+    private final EmployeeRepository repository;
+    
+    @Autowired
+    public DatabaseLoader(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+    
+    @Override
+    public void run(String... strings) throws Exception {
+        this.repository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+    }
+}
